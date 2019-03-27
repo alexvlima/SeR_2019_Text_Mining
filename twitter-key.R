ccc5b8a30b3a5aabd2f38414f03881582381d217
Alexandre V Lima <alexvlima83@gmail.com>
2019-03-27 15:52:39 -0300

    Create twitter-key.R

---
+++ twitter-key.R
@@ -0,0 +1,17 @@
+############
+### KEYS ###
+############
+
+library(twitteR)
+
+# Generate by mt personal user profile #
+
+api_key <- "XXXXXXXXXXX"
+api_secret <- "XXXXXXXXXXX"
+access_token <- "XXXXXXXXXXX"
+access_token_secret <- "XXXXXXXXXXX"
+
+setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
+1
+
+rm(list = ls())
